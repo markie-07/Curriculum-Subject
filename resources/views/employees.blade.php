fe67440b6e500026f144c39925b442d9768e8f4f
MARIE JOAN DE GUZMAN <145414986+mariejon03@users.noreply.github.com>
2026-02-07 01:47:47 +0800

    ...

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -644,26 +644,7 @@
         </div>
 
         <script>
-        // Password visibility toggle function
-        function togglePasswordVisibility(inputId, button) {
-            const input = document.getElementById(inputId);
-            const svg = button.querySelector('svg');
-            
-            if (input.type === 'password') {
-                input.type = 'text';
-                // Change to "eye-slash" icon (hidden)
-                svg.innerHTML = `
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.878 9.878L3 3m6.878 6.878L21 21"></path>
-                `;
-            } else {
-                input.type = 'password';
-                // Change back to "eye" icon (visible)
-                svg.innerHTML = `
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                `;
-            }
-        }
+
 
         document.addEventListener('DOMContentLoaded', function () {
             // Modal elements
@@ -923,4 +904,26 @@
         </script>
     @endif
 </div>
+<script>
+    // Global Password visibility toggle function
+    function togglePasswordVisibility(inputId, button) {
+        const input = document.getElementById(inputId);
+        const svg = button.querySelector('svg');
+        
+        if (input.type === 'password') {
+            input.type = 'text';
+            // Change to "eye-slash" icon (hidden)
+            svg.innerHTML = `
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.878 9.878L3 3m6.878 6.878L21 21"></path>
+            `;
+        } else {
+            input.type = 'password';
+            // Change back to "eye" icon (visible)
+            svg.innerHTML = `
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+            `;
+        }
+    }
+</script>
 @endsection
