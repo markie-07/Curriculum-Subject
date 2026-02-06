2a605eb649b2185e0f69873e5f4716e137e29ffd
MARIE JOAN DE GUZMAN <145414986+mariejon03@users.noreply.github.com>
2026-02-07 03:13:53 +0800

    Fix migrations checks for existing tables and columns

--- database/migrations/2026_02_06_175319_add_profile_picture_to_users_table.php
+++ database/migrations/2026_02_06_175319_add_profile_picture_to_users_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_picture')->nullable()->after('email');
-        });
+        if (!Schema::hasColumn('users', 'profile_picture')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('profile_picture')->nullable()->after('email');
+            });
+        }
     }
 
     /**
