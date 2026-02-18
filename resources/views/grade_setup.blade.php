354f85ee49a108eb4334d3676319dc6ec23fa8b5
ola <164900161+Ola-xooox@users.noreply.github.com>
2026-02-18 22:05:22 +0800

    remove manage template button temporary

--- resources/views/grade_setup.blade.php
+++ resources/views/grade_setup.blade.php
@@ -177,13 +177,14 @@
                                 </div>
                             </div>
 
+                            <!--
                             <a href="{{ route('grading_templates.index') }}" class="flex items-center gap-2 text-sm font-semibold text-blue-600 hover:text-blue-800 transition-colors py-2 px-3 rounded-lg hover:bg-blue-50 border border-transparent hover:border-blue-200">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                     <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" />
                                 </svg>
                                 Manage Templates
                             </a>
-
+                            -->
 
                             <button id="add-grade-component-btn" type="button" style="display: none;" class="flex items-center gap-2 text-sm font-semibold text-indigo-600 hover:text-indigo-800 transition-colors py-2 px-3 rounded-lg hover:bg-indigo-50">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
